add7c3898e944b799a07ff5b536394f0296732b0
Twan-M <twan@mellink.me>
2023-04-28 00:38:49 +0200

    Main to connect to DB

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"GoScraper/repositories"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if os.Getenv("APP_MODE") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	if repositories.Connected() {
+		log.Println("Starting Database Connection")
+	}
+}
